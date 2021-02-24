0195cdc1ac3529eb947e2ac397b86ed245356d4e
SonicYeager <kimstone.yeager@gmail.com>
2021-02-24 11:37:39 +0100

    deployed;

---
+++ SLFGame/deploy.bat
@@ -0,0 +1,1 @@
+.\windeployqt.exe --relocatable --qmldir H:\Repositories\Anforderungsanalyse\SLFGame\SLFGameUI H:\Repositories\Anforderungsanalyse\SLFGame\build-SLFGameUI-Desktop_Qt_6_0_1_MSVC2019_64bit-Release\release\SLFGameUI.exe
